18ad25902b447d4d351cd996c47aa1ef7399998b
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2022-09-09 11:26:08 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -1,1 +1,28 @@
-// место для вашего кода
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+enum class QueryType {
+    NewBus,
+    NewStop
+};
+
+struct Query {
+    QueryType type;
+    string stop_name;
+    string bus_name;
+    vector<string> stops;
+    double latitude;
+    double longitude;
+    bool is_round;
+    unordered_map<string, int> range_to_close_stop;
+};
+
+istream& operator>>(istream& is, Query& q);
+
+vector<Query> BuildDataBase();
