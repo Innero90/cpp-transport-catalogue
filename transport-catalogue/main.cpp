b72fcd09706b2f04b1f7c5e86bf99a3d6489b6d5
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2022-09-09 11:26:20 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,8 @@
-// место для вашего кода
+#include "transport_catalogue.h"
+#include "stat_reader.h"
+
+int main() {
+    auto data_base = move(BuildDataBase());
+    TransportCatalogue transport_catalogue(data_base);
+    PrintInfo(transport_catalogue);
+}
