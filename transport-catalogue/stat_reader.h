ef0b26cc436a547bc9d61c5151c1251a9fe76a21
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-01-27 10:52:59 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -5,8 +5,6 @@
 
 #include "transport_catalogue.h"
 
-using namespace std;
-
-void PrintBusInfo(string& bus, tuple<int, int, double> result);
-void PrintStopInfo(string& stop, set<string> result);
+void PrintBusInfo(const string& bus, tuple<int, int, double> result);
+void PrintStopInfo(const string& stop, set<string> result);
 void PrintInfo(TransportCatalogue& transport_catalogue);
