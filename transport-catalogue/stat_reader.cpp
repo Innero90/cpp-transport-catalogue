a72f63b44d133fbae4a3e3615565f9b8b506e474
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-01-27 10:52:46 +0300

    Update stat_reader.cpp

--- transport-catalogue/stat_reader.cpp
+++ transport-catalogue/stat_reader.cpp
@@ -1,24 +1,23 @@
-
 #include "stat_reader.h"
 
-void PrintBusInfo(string& bus, tuple<int, int, int, double> result) {
+void PrintBusInfo(const string& bus_name, tuple<int, int, int, double> result) {
     if (get<0>(result) != 0) {
-        cout << "Bus "s << bus << ": "s << get<0>(result) << " stops on route, "s
+        cout << "Bus "s << bus_name << ": "s << get<0>(result) << " stops on route, "s
             << get<1>(result) << " unique stops, "s << get<2>(result) << " route length, "s << get<3>(result) << " curvature"s << endl;
     }
     else {
-        cout << "Bus "s << bus << ": not found"s << endl;
+        cout << "Bus "s << bus_name << ": not found"s << endl;
     }
 }
 
-void PrintStopInfo(string& stop, set<string> result) {
+void PrintStopInfo(const string& stop_name, set<string> result) {
     string first = *result.begin();
     if (first == "no buses"s || first == "not found"s) {
-        cout << "Stop " << stop << ": " << first << endl;
+        cout << "Stop " << stop_name << ": " << first << endl;
     }
     else {
         bool is_first = true;
-        cout << "Stop " << stop << ": buses ";
+        cout << "Stop " << stop_name << ": buses ";
         for (auto& text : result) {
             if (!is_first) {
                 cout << " ";
