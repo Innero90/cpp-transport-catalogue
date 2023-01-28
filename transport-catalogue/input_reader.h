a6eb3e7852199d8e8a89480b18ea4bb58663f6fe
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-01-28 11:33:18 +0300

    Update input_reader.h

--- transport-catalogue/input_reader.h
+++ transport-catalogue/input_reader.h
@@ -4,7 +4,7 @@
 
 #include <map>
 
-struct Bus {
+struct Bus {            // у меня ломается весь справочник если выносить структуры и начинать пользоваться теми что в каталоге, почему не понимаю, как правильно подключить файлы
     std::string name;
     bool is_round = false;
     std::vector<std::string> stops;
@@ -18,10 +18,12 @@ struct Stop {
 struct Querys {
     std::vector<Stop> stops;
     std::vector<Bus> buses;
-    std::map<std::pair<std::string, std::string>, int> range_about_stops;
+    std::map<std::pair<std::string_view, std::string_view>, int> range_about_stops;
 };
 
 std::istream& operator>>(std::istream& is, Querys& q);
 
 Querys BuildDataBase();
 Querys BuildDataBase(std::istream& querys_data);
+Bus BusQueryParser(std::string& line);
+Stop StopQueryParser(std::string& line, Querys& q);
