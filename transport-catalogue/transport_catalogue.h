b064f144998d7462cd8508077bc0a9dcd833640b
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-01-28 11:34:15 +0300

    Update transport_catalogue.h

--- transport-catalogue/transport_catalogue.h
+++ transport-catalogue/transport_catalogue.h
@@ -11,6 +11,34 @@
 
 using namespace std;
 
+struct StopInfo {
+    StopInfo(Stop stop) :
+        name_(stop.name) {
+        coord.lat = stop.coord.lat;
+        coord.lng = stop.coord.lng;
+    }
+
+    string name_;
+    Coordinates coord;
+};
+
+struct RouteInfo {
+    RouteInfo(Bus bus) :
+        name_(bus.name), is_round_(bus.is_round) {
+    }
+
+    string_view name_;
+    bool is_round_;
+    deque<StopInfo*> stops_;
+};
+
+struct BusInfoResult {
+    int stops_on_route = 0;
+    int unique_stops = 0;
+    int route_length = 0;
+    double curvature = 0.0;
+};
+
 class TransportCatalogue {
 public:
     TransportCatalogue(Querys data_base);
@@ -18,39 +46,18 @@ public:
     void AddStop(Stop stop);
     void AddBus(Bus bus);
 
-    tuple<int, int, int, double> GetBusInfo(string& bus);
+    BusInfoResult GetBusInfo(string& bus);
     set<string> GetStopInfo(string& stop);
 
-    int StopsCountInRoute(string& bus);
-    int UniqStopsCountInRoute(string& bus);
+    int GetStopsCountInRoute(string& bus);
+    int GetUniqStopsCountInRoute(string& bus);
     int ComputeMeterRouteLenght(string& bus);
     double ComputeCoordRouteLenght(string& bus);
 
 private:
-    struct StopInfo {
-        StopInfo(Stop stop) :
-            name_(stop.name) {
-            coord.lat = stop.coord.lat;
-            coord.lng = stop.coord.lng;
-        }
-
-        string name_;
-        Coordinates coord;
-    };
-
-    struct RouteInfo {
-        RouteInfo(Bus bus) :
-            name_(bus.name), is_round_(bus.is_round) {
-        }
-
-        string name_;
-        bool is_round_;
-        deque<StopInfo*> stops_;
-    };
-
     deque<StopInfo> stops_;
     unordered_map<string_view, StopInfo*> stops;
     deque<RouteInfo> routes_;
     unordered_map<string_view, RouteInfo*> routes;
-    map<pair<string, string>, int> range_about_stops;
+    map<pair<string_view, string_view>, int> range_about_stops;   //если поставить unordered_map,то ломается словарь, не понимаю почему? так же если ставить string_view ломается результат метода GetStopInfo
 };
