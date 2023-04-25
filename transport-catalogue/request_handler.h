6d94965370035c8d2ec9b922e223ecebf72fb0c3
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-04-25 18:02:16 +0300

    Create request_handler.h

---
+++ transport-catalogue/request_handler.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "json.h"
+#include "map_renderer.h"
+#include "transport_catalogue.h"
+
+namespace transport {
+
+class RequestHandler {
+public:
+    RequestHandler(const TransportCatalogue& db, renderer::MapRenderer& renderer);
+
+    std::optional<BusStat> GetBusStat(const std::string& bus_name) const;
+
+    const std::set<std::string_view>* GetBusesThroughStop(const std::string& stop_name) const;
+
+    const svg::Document& RenderMap() const;
+
+    json::Document GetJsonResponse(const json::Array& requests) const;
+
+private:
+    const TransportCatalogue& db_;
+    renderer::MapRenderer& renderer_;
+};
+
+
+} // transport
