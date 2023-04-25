73fe607821b4a402c682c31b95be31ae8050065e
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-04-25 18:00:51 +0300

    Create json_reader.h

---
+++ transport-catalogue/json_reader.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <iostream>
+
+#include "map_renderer.h"
+#include "transport_catalogue.h"
+#include "json.h"
+#include "request_handler.h"
+
+namespace transport {
+
+class JsonReader {
+private:
+    json::Document json_doc_{json::Node{nullptr}};
+
+    const json::Array& GetBaseRequests() const;
+
+    const json::Dict& GetRenderSettings() const;
+
+    const json::Array& GetStatRequests() const; 
+
+    svg::Color GetColorFromNode(const json::Node& n) const;
+
+    renderer::RenderSettings DictToRenderSettings(const json::Dict& settings_dict) const;
+
+    parsed::Bus DictToBus(const json::Dict& bus_dict) const;
+
+    std::pair<parsed::Stop, parsed::Distances> DictToStopDists(const json::Dict& stop_dict) const;
+
+public:
+    JsonReader(std::istream& input);
+
+    renderer::MapRenderer GetRenderer(const TransportCatalogue& catalogue) const;
+
+    void FillCatalogue(TransportCatalogue& catalogue) const;
+
+    void PrintJsonResponse(const RequestHandler& handler, std::ostream& out) const;
+};
+
+} // transport
