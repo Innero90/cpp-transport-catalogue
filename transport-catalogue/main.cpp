e0c90520e5f54c74c5e9470548d61492d7535872
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-04-25 18:01:17 +0300

    Update main.cpp

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
-#include <sstream>
 
-#include "stat_reader.h"
+#include "json_reader.h"
 
 using namespace std;
 
 int main() {
-    istringstream querys_data{
-        "Stop Tolstopaltsevo: 55.611087, 37.20829, 3900m to Marushkino\n"
-        "Stop Marushkino: 55.595884, 37.209755, 9900m to Rasskazovka, 100m to Marushkino\n"
-        "Bus 256: Biryulyovo Zapadnoye > Biryusinka > Universam > Biryulyovo Tovarnaya > Biryulyovo Passazhirskaya > Biryulyovo Zapadnoye\n"
-        "Bus 750: Tolstopaltsevo - Marushkino - Marushkino - Rasskazovka\n"
-        "Stop Rasskazovka: 55.632761, 37.333324, 9500m to Marushkino\n"
-        "Stop Biryulyovo Zapadnoye: 55.574371, 37.6517, 7500m to Rossoshanskaya ulitsa, 1800m to Biryusinka, 2400m to Universam\n"
-        "Stop Biryusinka: 55.581065, 37.64839, 750m to Universam\n"
-        "Stop Universam: 55.587655, 37.645687, 5600m to Rossoshanskaya ulitsa, 900m to Biryulyovo Tovarnaya\n"
-        "Stop Biryulyovo Tovarnaya: 55.592028, 37.653656, 1300m to Biryulyovo Passazhirskaya\n"
-        "Stop Biryulyovo Passazhirskaya: 55.580999, 37.659164, 1200m to Biryulyovo Zapadnoye\n"
-        "Bus 828: Biryulyovo Zapadnoye > Universam > Rossoshanskaya ulitsa > Biryulyovo Zapadnoye\n"
-        "Stop Rossoshanskaya ulitsa: 55.595579, 37.605757\n"
-        "Stop Prazhskaya: 55.611678, 37.603831\n"
-    };
+    using namespace transport;
+    
+    TransportCatalogue catalogue;
+    JsonReader reader(cin);
 
-    auto data_base = move(BuildDataBase());
-    TransportCatalogue transport_catalogue(move(data_base));
-    PrintInfo(transport_catalogue);
+    reader.FillCatalogue(catalogue);
+
+    renderer::MapRenderer renderer = reader.GetRenderer(catalogue);
+
+    RequestHandler handler(catalogue, renderer);
+
+    reader.PrintJsonResponse(handler, cout);
 }
