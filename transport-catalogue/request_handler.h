eb7875827d8543668abb08749ffb6ff9a8eadf9b
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-09-14 16:52:21 +0300

    Update request_handler.h

--- transport-catalogue/request_handler.h
+++ transport-catalogue/request_handler.h
@@ -1,27 +1,59 @@
+/*
+ * Назначение модуля: обработчик запросов к базе, содержащего логику,
+ * которую не хотелось бы помещать ни в transport_catalogue, ни в json reader.
+ *
+ *    request_handler - интерфейс("Фасад) транспортного справочника (двоичные данные)
+ *        json_reader - интерфейс работы с данными формата json + добавление данных
+ */
+
 #pragma once
 
-#include "json_builder.h"
-#include "map_renderer.h"
 #include "transport_catalogue.h"
+#include "map_renderer.h"
 
-namespace transport {
+#include <unordered_set>    // для типа данных в RequestHandler
+#include <optional>         // для типа данных в RequestHandler
+#include <string_view>      // для типа данных в RequestHandler
+#include <map>              // для передачи словаря всех маршрутов
 
-class RequestHandler {
+namespace detail
+{
+// Преобразует 1 строку в 2, разделенные count по порядку символом-разделителем
+std::pair<std::string_view, std::string_view> Split(std::string_view, char, int count = 1);
+// Удаляет пробелы (переводы строк, табуляцию и т.п.) из начала строки
+std::string_view Lstrip(std::string_view);
+// Удаляет пробелы (переводы строк, табуляцию и т.п.) с конца строки
+std::string_view Rstrip(std::string_view);
+// Удаляет пробелы (переводы строк, табуляцию и т.п.) со всех сторон строки
+std::string_view TrimString(std::string_view);
+}
+
+namespace transport_catalogue
+{
+// Класс RequestHandler играет роль Фасада, упрощающего взаимодействие JSON reader-а
+// с другими подсистемами приложения.
+// См. паттерн проектирования Фасад: https://ru.wikipedia.org/wiki/Фасад_(шаблон_проектирования)
+class RequestHandler
+{
 public:
-    RequestHandler(const TransportCatalogue& db, renderer::MapRenderer& renderer);
+    // MapRenderer понадобится в следующей части итогового проекта
+    RequestHandler(const TransportCatalogue& tc, map_renderer::MapRenderer& mr) : tc_(tc), mr_(mr)
+    {}
 
-    std::optional<BusStat> GetBusStat(const std::string& bus_name) const;
+    // Возвращает информацию о маршруте
+    const std::optional<RouteStatPtr> GetRouteInfo(const std::string_view& bus_name) const;
 
-    const std::set<std::string_view>* GetBusesThroughStop(const std::string& stop_name) const;
+    // Возвращает маршруты, проходящие через остановку
+    //const std::unordered_set<BusPtr>* GetBusesByStop(const std::string_view& stop_name) const;
+    const std::optional<StopStatPtr> GetBusesForStop(const std::string_view& stop_name) const;
 
-    const svg::Document& RenderMap() const;
-
-    json::Document GetJsonResponse(const json::Array& requests) const;
+    // Возвращает SVG документ, сформированный map_renderer
+    svg::Document GetMapRender() const;
 
 private:
-    const TransportCatalogue& db_;
-    renderer::MapRenderer& renderer_;
+    // RequestHandler использует агрегацию объектов "Транспортный Справочник" и "Визуализатор Карты"
+    const TransportCatalogue& tc_;
+
+    map_renderer::MapRenderer& mr_;
 };
-
-
-} // transport
+}
