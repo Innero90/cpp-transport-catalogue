3e66a9d2e20c3392aa5f067d43633301d77e13d8
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-09-14 16:47:03 +0300

    Update geo.cpp

--- transport-catalogue/geo.cpp
+++ transport-catalogue/geo.cpp
@@ -1,21 +1,38 @@
-#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES  // Для константы Пи
+
 #include "geo.h"
 
 #include <cmath>
 
-namespace geo {
+namespace geo
+{
 
-double ComputeDistance(Coordinates from, Coordinates to) {
+bool Coordinates::operator==(const Coordinates& other) const
+{
+    return lat == other.lat && lng == other.lng;
+}
+
+bool Coordinates::operator!=(const Coordinates& other) const
+{
+    return !(*this == other);
+}
+
+std::size_t CoordinatesHasher::operator()(const Coordinates& coords) const
+{
+    return static_cast<std::size_t>(coords.lat + 37 * coords.lng);
+}
+
+double ComputeDistance(Coordinates from, Coordinates to)
+{
     using namespace std;
-    if (from == to) {
+    if (from == to)
+    {
         return 0;
     }
     static const double dr = M_PI / 180.;
-    static const double earth_radius = 6371000;
-    
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * earth_radius;
+                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr)) 
+        * EARTH_RADIUS;
 }
 
-}  // namespace geo
+}
