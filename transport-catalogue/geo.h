e5fb60a44676c44d23544b5a414be9b2bb1cb55d
Rassolenko Konstantin <103990960+Innero90@users.noreply.github.com>
2023-09-14 16:47:16 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -1,18 +1,34 @@
 #pragma once
 
-namespace geo {
+// Этот include нужно включать если компилятор ругается на необъявленный тип size_t
+// Обычно size_t определяется в заголовках с контейнерами STL, но при их отсутствии 
+// может вызвать ошибку. В этом случае нужно добавить #include <cstddef>
+#include <cstddef>
 
-struct Coordinates {
-    double lat; // Широта
-    double lng; // Долгота
-    bool operator==(const Coordinates& other) const {
-        return lat == other.lat && lng == other.lng;
-    }
-    bool operator!=(const Coordinates& other) const {
-        return !(*this == other);
-    }
+namespace geo
+{
+
+// Радиус Земли, метров
+const int EARTH_RADIUS = 6371000;
+
+// Структура, определяющая положение точки в пространстве
+// и методы работы с ней
+struct Coordinates
+{
+    double lat;  // Широта
+    double lng;  // Долгота
+    bool operator==(const Coordinates&) const;
+    bool operator!=(const Coordinates&) const;
 };
 
+class CoordinatesHasher
+{
+public:
+    std::size_t operator()(const Coordinates&) const;
+};
+
+// Функция рассчитывает расстояние между остановками по поверхности
+// земного шара
 double ComputeDistance(Coordinates from, Coordinates to);
 
 }  // namespace geo
